6bea8bed5882e035ae8b1e43b86fc011e16e1138
ritrafa <rit.rafa1@gmail.com>
2024-06-18 23:22:15 -0400

    Updating config for env variable

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    NEXT_PUBLIC_TL_API: process.env.NEXT_PUBLIC_TL_API,
+  },
 }
 
 module.exports = nextConfig
